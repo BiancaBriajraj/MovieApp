45d315d0f2e3ae3c1c5193451936e8f36739d993
Bianca <biancabriajraj@gmail.com>
2021-07-08 16:36:14 +0200

    Fetching from APIs, created fragments

---
+++ app/src/main/java/com/example/movieapp/viewModel/FullDetailsViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.movieapp.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.movieapp.model.models.MovieFullDetails
+import com.example.movieapp.model.servies.MovieFullDetailsApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class FullDetailsViewModel : ViewModel() {
+
+    private val BASE_URL = "https://api.themoviedb.org/3/"
+    private val API_KEY = "092ef080e5f1d57b6101f71603911376"
+
+    private val _listOfFullMovieDetails = MutableLiveData<MovieFullDetails>()
+    val listOfFullMovieDetails: LiveData<MovieFullDetails> get() = _listOfFullMovieDetails
+
+    val loading = MutableLiveData<Boolean>()
+    val error = MutableLiveData<Boolean>()
+    fun getInfo() {
+        retrieveList()
+    }
+
+    private fun retrieveList() {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+        loading.value = true
+
+        val apiService = retrofit.create(MovieFullDetailsApiService::class.java)
+        val mCall: Call<MovieFullDetails> = apiService.getFullMovieInfo(550, API_KEY)
+        mCall.enqueue(object : Callback<MovieFullDetails> {
+            override fun onResponse(call: Call<MovieFullDetails>, response: Response<MovieFullDetails>) {
+                if (response.isSuccessful) {
+                    _listOfFullMovieDetails.value = response.body()!!
+                    loading.value = false
+
+                } else {
+                    loading.value = false
+                }
+            }
+            override fun onFailure(call: Call<MovieFullDetails>, t: Throwable) {
+                loading.value = false
+                error.value = true
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/viewModel/SummDetailsViewModel.kt
@@ -0,0 +1,82 @@
+package com.example.movieapp.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.movieapp.model.MovieSummDetails
+import com.example.movieapp.model.servies.MovieSummDetailsApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SummDetailsViewModel : ViewModel() {
+    private val BASE_URL = "https://api.themoviedb.org/3/"
+    private val API_KEY = "092ef080e5f1d57b6101f71603911376"
+
+    private val _listSearched = MutableLiveData<MovieSummDetails>()
+    val listSearch: LiveData<MovieSummDetails> get() = _listSearched
+
+    private val _listAllMovies = MutableLiveData<MovieSummDetails>()
+    val listAllMovies: LiveData<MovieSummDetails> get() = _listAllMovies
+
+    val loading = MutableLiveData<Boolean>()
+    val error = MutableLiveData<Boolean>()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    fun getSearchedMovieDetails(movieName: String) {
+        findMovie(movieName)
+    }
+
+    fun getAdult(adultIncluded: Boolean) {
+        sortMovies(adultIncluded)
+    }
+
+    private fun findMovie(name: String) {
+        loading.value = true
+        val apiService = retrofit.create(MovieSummDetailsApiService::class.java)
+        val mCall: Call<MovieSummDetails> = apiService.getSearchedMovieInfo(API_KEY, name)
+        mCall.enqueue(object : Callback<MovieSummDetails> {
+            override fun onResponse(call: Call<MovieSummDetails>, response: Response<MovieSummDetails>) {
+                if (response.isSuccessful) {
+                    _listSearched.value = response.body()!!
+                    loading.value = false
+                } else {
+                    loading.value = false
+                }
+
+            }
+            override fun onFailure(call: Call<MovieSummDetails>, t: Throwable) {
+                loading.value = false
+                error.value = true
+            }
+        })
+    }
+
+    private fun sortMovies(adultIncluded: Boolean) {
+        loading.value = true
+        val apiService = retrofit.create(MovieSummDetailsApiService::class.java)
+        val mCall: Call<MovieSummDetails> = apiService.getMovies(API_KEY, adultIncluded, 1)
+        mCall.enqueue(object : Callback<MovieSummDetails> {
+            override fun onResponse(call: Call<MovieSummDetails>, response: Response<MovieSummDetails>) {
+                if (response.isSuccessful) {
+                    _listAllMovies.value = response.body()!!
+                    loading.value = false
+
+                } else {
+                    loading.value = false
+                }
+
+            }
+            override fun onFailure(call: Call<MovieSummDetails>, t: Throwable) {
+                loading.value = false
+                error.value = true
+            }
+        })
+    }
+}
