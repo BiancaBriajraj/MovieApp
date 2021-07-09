a949a49ba1f8d1d6cf222e3b6dca18d0f0a41762
Bianca <biancabriajraj@gmail.com>
2021-07-09 14:39:29 +0200

    Setting up recyclerviews

--- app/src/main/java/com/example/movieapp/view/MainScreenFrgament.kt
+++ app/src/main/java/com/example/movieapp/view/MainScreenFrgament.kt
@@ -5,56 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentListOfMoviesBinding
+import com.example.movieapp.databinding.FragmentMainScreenBinding
+import com.example.movieapp.viewModel.SummDetailsViewModel
+import kotlinx.android.synthetic.main.fragment_main_screen.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+private lateinit var binding: FragmentMainScreenBinding
+private lateinit var viewModel: SummDetailsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+       binding = FragmentMainScreenBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(SummDetailsViewModel::class.java)
+        viewModel.getSearchedMovieDetails("Hulk")
+
+        viewModel.listSearch.observe(viewLifecycleOwner, { list ->
+               Glide.with(this)
+                   .load("https://image.tmdb.org/t/p/w500/${list.results[0].posterPath}")
+                   .into(imageView8)
+
+            Glide.with(this)
+                .load("https://image.tmdb.org/t/p/w500/${list.results[1].posterPath}")
+                .into(imageView9)
+        })
+
+
+
+
+
     }
+
 }
